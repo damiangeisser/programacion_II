80888c100c6dcb7ffecbb4afa12d5053c8e6ca7a
Damián Geisser <47353947+damiangeisser@users.noreply.github.com>
2020-04-14 23:34:19 -0300

    Add files via upload

---
+++ Ejercicio_Integrador_Clase_5/Repaso/Estante.cs
@@ -0,0 +1,110 @@
+﻿using Ejercicio_Integrador;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso
+{
+    public class Estante
+    {
+        private Producto[] productos;
+        private int ubicacionEstante;
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+
+            for (int i = 0; i < productos.Length; i++)
+            {
+                productos[i] = new Producto("", "", 0);
+            }
+        }
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+        public Producto[] GetProductos()
+        {
+            return this.productos;
+        }
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder contenidoEstante = new StringBuilder();
+
+            contenidoEstante.Append("\nEl estante posee capacidad para " + e.productos.Length.ToString() + " productos.\n");
+            contenidoEstante.Append("Los productos en stock son:\n\n");
+
+            foreach (var producto in e.productos)
+            {
+               contenidoEstante.Append(Producto.MostrarProducto(producto));
+            }
+
+            return contenidoEstante.ToString();
+        }
+        public static bool operator ==(Estante e, Producto p)
+        {
+            bool isMatch = false;
+
+            foreach (var producto in e.productos)
+            {
+                if((string)producto == (string)p)
+                {
+                    isMatch = true;
+                    break;
+                }
+            }
+
+            return isMatch;
+        }
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e==p);
+        }
+        public static bool operator +(Estante e, Producto p)
+        {
+            bool inStock;
+            bool reStock = false;
+
+            inStock = e == p;
+
+            if (!inStock)
+            {
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if ((string)e.productos[i] == "")
+                    {
+                        e.productos[i] = p;
+
+                        reStock = true;
+
+                        break;
+                    }
+                }
+            }
+
+            return reStock;
+        }
+        public static Estante operator -(Estante e, Producto p)
+        {
+            bool inStock;
+
+            inStock = e == p;
+
+            if (inStock)
+            {
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if ((string)e.productos[i] == (string)p)
+                    {
+                        e.productos[i] = new Producto("","",0);
+
+                        break;
+                    }
+                }
+            }
+
+            return e;
+        }
+    }
+}
---
+++ Ejercicio_Integrador_Clase_5/Repaso/Producto.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Integrador
+{
+    public class Producto
+    {
+        private string codigoDeBarras;
+        private string marca;
+        private float precio;
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.marca = marca;
+            this.codigoDeBarras = codigo;
+            this.precio = precio;
+        }
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+        public static string MostrarProducto(Producto p)
+        {
+            return "Marca: " + p.marca + "\nCódigo: " + p.codigoDeBarras + "\nPrecio: $" + p.precio.ToString() + "\n\n";
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarras;
+        }
+        public static bool operator ==(Producto p, string marca)
+        {
+            bool isMatch = false;
+
+            if(p.marca == marca)
+            {
+                isMatch = true;
+            }
+
+            return isMatch;
+        }
+        public static bool operator !=(Producto p, string marca)
+        {
+            return !(p==marca);
+        }
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            bool isMatch = false;
+
+            if (p1.codigoDeBarras == p2.codigoDeBarras)
+            {
+                isMatch = true;
+            }
+
+            return isMatch;
+        }
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+
+    }
+}
